3161e35644525fa5bc4ffbdb1b6629142feeb6ae
rakaadli <Raka_adli@hotmail.com>
2019-01-25 14:40:49 +0700

    haha

--- tes.py
+++ tes.py
@@ -1,4 +1,16 @@
 print('tes 1 2 3')
+print('tes 3 4 5')
+print('tes 1 2 3')
+print('tes 3 4 5')
+print('tes 1 2 3')
+print('tes 3 4 5')
 
 # checkpoint =  commit
 # Final 1.0 = commit
+# git add
+# git commit
+# git checkout (kode)
+# git checkout master
+# git reset (kode)
+# git reset (kode) --hard
+# git add. dan git commit
